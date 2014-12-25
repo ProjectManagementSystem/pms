f8fbe826e10e5bf43a3e43c37f2204bf66fca1e1
Doruk-Aksoy <thunderwrath2001@gmail.com>
2014-12-25 22:45:17 +0200

    Removed hash, relies on AUTO_INCREMENT from database for uid and primarykey

--- server/register.py
+++ server/register.py
@@ -1,19 +1,19 @@
 # [DA] commits
-import time
-import random
-import socket
-import hashlib
+#import time
+#import random
+#import socket
+#import hashlib
 import MySQLdb
-
+"""
 #adds more randomness to the guid
 UID_CONST1 = 0x7FFFFFFF
 UID_CONST2 = 0x1337B8D8
 
 def guid(*args):
-    """
-    Generates a universally unique ID.
-    Any arguments only create more randomness.
-    """
+
+    # Generates a universally unique ID.
+    # Any arguments only create more randomness.
+
     t = long(time.time() * 1000)
     r = long(random.random() * 100000000000000000L)
     try:
@@ -25,24 +25,20 @@ def guid(*args):
     data = hashlib.md5(data).hexdigest()
 
     return data[:8]
-
+"""
 # var size limits: userid => [8]??, name - surname - phone - facebookid => char[128], password => char[512] primarykey (userid)
 
-def register (u_name, u_surname, u_mail, u_pass, u_phone, u_facebookid, u_regdate, u_primarykey):
-    u_uniqueid = guid(UID_CONST1, UID_CONST2)
-    print "Adding user with name " + u_name + " with generated id " + str(u_uniqueid)
+def register (u_name, u_surname, u_mail, u_pass, u_phone, u_facebookid, u_regdate):
     # connect to database, send the params
     db = MySQLdb.connect(host = "localhost", user = "Doruk", passwd = "bestpassever", db = "pms")
     # can take queries now
     cur = db.cursor()
     # first, try to insert this info to the database
     try:
-        cur.execute("""INSERT INTO USERINFO(NAME, SURNAME, MAIL, PASS, PHONE, FACEBOOKID, REGISTERDATE, PRIMARYKEY)
-                                   VALUES(%s, %s, %s, %s, %s, %s, %s, %s)""", (u_name, u_surname, u_mail, u_pass, u_phone, u_facebookid, u_regdate, u_primarykey))
+        cur.execute("""INSERT INTO USERINFO(NAME, SURNAME, MAIL, PASS, PHONE, FACEBOOKID, REGISTERDATE)
+                                   VALUES(%s, %s, %s, %s, %s, %s, %s)""", (u_name, u_surname, u_mail, u_pass, u_phone, u_facebookid, u_regdate))
         db.commit()
     except:
         db.rollback()
         return false
     return true
-
-register("John", "Smith", "test@test.com", "johndoe", "testnum", "testid", "testdate", 1)
