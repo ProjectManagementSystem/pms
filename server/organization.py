de98a2e5b35e720dd778bcf96678b2d67e5495ff
0sk1 <ozturk.osman@metu.edu.tr>
2014-12-25 21:47:01 +0200

    MySQL Bağlantı Eksikleri Giderldi, Denemesi Yapıldı.

--- server/organization.py
+++ server/organization.py
@@ -1,25 +1,34 @@
+from logindb import loginwrite as info
 import MySQLdb
 
 
 
-def CreateOrganization(name, email, webpage, phone, logo_path):
-
-	db = MySQLdb.connect(host="localhost",
-						 user="john", 
-						  passwd="megajonhy", 
-						  db="john dalton")
+def CreateOrganization(name, email, webpage, phone, logo_path, debug = False):
 	
+	db = MySQLdb.connect(host=info.host,
+						 user=info.username, 
+						  passwd=info.password, 
+						  db=info.db)
 	
-	
-	cur = db.cursor() 	
-	sql = "INSERT INTO organization(name, email, webpage, phone, logopath) VALUES ('@1', '@2', '@3', '@4', '@5')"
+	cur = db.cursor()
+	sql = "INSERT INTO organization(oid,name, email, webpage, phone, logopath) VALUES (2,'@1', '@2', '@3', @4, '@5')"
 	sql = sql.replace("@1",name)
 	sql = sql.replace("@2",email)
 	sql = sql.replace("@3",webpage)
-	sql = sql.replace("@4",phone)
+	sql = sql.replace("@4",str(phone))
 	sql = sql.replace("@5",logo_path)
-	try:
+	result = False
+	if( debug == False):
+		try:
+			cur.execute(sql)
+			db.commit()
+			result = True
+		except:
+			result = False
+	else:
 		cur.execute(sql)
-	except:
-		return False;
-	return True;
+		db.commit()
+		result = True
+	db.close()
+	return result;
+CreateOrganization("A","a@b.com","www.x.com",123123,"logo.gif")
