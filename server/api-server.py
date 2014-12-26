225b4a590e3321836e129a457e822c9a1b953414
0sk1 <ozturk.osman@metu.edu.tr>
2014-12-26 19:27:35 +0200

    account_login(), account_logout()

--- server/api-server.py
+++ server/api-server.py
@@ -3,8 +3,14 @@ requests:
 http://localhost:5000/api/v1.0/<main_function>/<sub_function>/
 
 
+
+@brief          checks username, password and crate a session
 http://localhost:5000/api/v1.0/account/login
+
+@brief          clear the sessions
 http://localhost:5000/api/v1.0/account/logout
+
+@brienf         client registration
 http://localhost:5000/api/v1.0/account/register
 
 http://localhost:5000/api/v1.0/organization/create
@@ -12,18 +18,16 @@ http://localhost:5000/api/v1.0/organization/delete
 http://localhost:5000/api/v1.0/organization/update
 
 
-ex:
-curl -i -H "Content-Type: application/json" -d '{"tag":"data.."}' http://localhost:5000/...
-
 
 """
+
 
 from flask import Flask, jsonify, abort, request, make_response, url_for
 
 flask_app = Flask(__name__)
 
 @flask_app.errorhandler(404)
-def not_found(error = 404):
+def not_found(error):
     return make_response(jsonify( { 'error': 'request not found' } ), 404)
 
 
@@ -33,29 +37,61 @@ def not_found(error):
 
 @flask_app.route('/api/v1.0/<string:mainFunction>/<string:subFunction>/')
 def APIStart( mainFunction, subFunction ):
-    
+    if not request.json:
+        abort(404)
+        
     func_list = [("account",account),("organization",organization)]
     for f in func_list:
         if( f[0] == mainFunction ):
             return f[1]( subFunction )
             
-    return not_found()
+    return abort(404)
 
 
 def account(subFunction):
-    func_list = [("login",t_function),("register",t_function), ("logout",t_function)]
+    func_list = [("login",account_login),("register",t_function), ("logout",account_logout)]
     for f in func_list:
         if( f[0] == subFunction ):
             return f[1]()
-    return not_found()
+    return abort(404)
     
 def organization(subFunction):
     func_list = [("create",t_function),("delete",t_function), ("update",t_function)]
     for f in func_list:
         if( f[0] == subFunction ):
             return f[1]()
-    return not_found()
+    return abort(404)
+
+
+
 def t_function():
     return "still has a lot of things to do."
+
+def account_login():
+    
+    
+    username = request.json['username']
+    password = request.json['password']
+    if( len(username) == 0 or
+        len(password) == 0):
+        HandleError( "Username or password is empty" )
+        
+        
+    import login
+    res = login.login(username,password)
+    if( res == True ):
+        session['username'] = username
+        return jsonify( { 'login': "True" } ), 201
+    else:
+        return jsonify( { 'login': "False" } ), 201
+
+def account_logout():
+    session.pop('username', None)
+    
+
+def HandleError( dest_error ):
+    return "error.."
+
+
 if __name__ == '__main__':
     flask_app.run(debug=True)
