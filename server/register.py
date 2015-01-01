6f5a63db97b55c32d290b9a01cef37638f960ac3
Doruk-Aksoy <thunderwrath2001@gmail.com>
2015-01-01 17:36:11 +0200

    Cleaner, working registration function
    Removed unused modules, removed unused comments, added debug mode,
    removed register date from parameters.

--- server/register.py
+++ server/register.py
@@ -1,46 +1,28 @@
 # [DA] commits
-#import time
-#import random
-#import socket
-#import hashlib
 import MySQLdb
-"""
-#adds more randomness to the guid
-UID_CONST1 = 0x7FFFFFFF
-UID_CONST2 = 0x1337B8D8
 
-def guid(*args):
-
-    # Generates a universally unique ID.
-    # Any arguments only create more randomness.
-
-    t = long(time.time() * 1000)
-    r = long(random.random() * 100000000000000000L)
-    try:
-        a = socket.gethostbyname(socket.gethostname())
-    except:
-        # if we can't get a network address, just imagine one
-        a = random.random()*100000000000000000L
-    data = str(t)+ ' ' + str(r) + ' ' + str(a) + ' ' + str(args)
-    data = hashlib.md5(data).hexdigest()
-
-    return data[:8]
-"""
 # var size limits: userid => [8]??, name - surname - phone - facebookid => char[128], password => char[512] primarykey (userid)
 from logindb import loginwrite as info
 
+debug = 1
 
-def register (u_name, u_surname, u_mail, u_pass, u_phone, u_facebookid, u_regdate):
+def register (u_name, u_surname, u_mail, u_pass, u_phone, u_facebookid):
     # connect to database, send the params
     db = MySQLdb.connect(host =  info.host, user = info.username , passwd = info.password , db = info.db)
     # can take queries now
     cur = db.cursor()
     # first, try to insert this info to the database
-    try:
-        cur.execute("""INSERT INTO USERINFO(NAME, SURNAME, MAIL, PASS, PHONE, FACEBOOKID, REGISTERDATE)
-                                   VALUES(%s, %s, %s, %s, %s, %s, %s)""", (u_name, u_surname, u_mail, u_pass, u_phone, u_facebookid, u_regdate))
+
+    if debug:
+        cur.execute("""INSERT INTO USERS(NAME, SURNAME, EMAIL, PASSWORD, PHONE, FACEBOOKID)
+                                   VALUES(%s, %s, %s, %s, %s, %s)""", (u_name, u_surname, u_mail, u_pass, u_phone, u_facebookid))
         db.commit()
-    except:
-        db.rollback()
-        return false
-    return true
+    else:
+        try:
+            cur.execute("""INSERT INTO USERS(NAME, SURNAME, EMAIL, PASSWORD, PHONE, FACEBOOKID)
+                                       VALUES(%s, %s, %s, %s, %s, %s)""", (u_name, u_surname, u_mail, u_pass, u_phone, u_facebookid))
+            db.commit()
+        except:
+            db.rollback()
+            return False
+    return True
